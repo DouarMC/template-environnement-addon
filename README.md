5c0205f486232fe8b3592292bf6dfd3a93e6e78a
DouarMC <marwane.ouaabouz@gmail.com>
2025-01-20 19:15:50 +0100

    Initialize Minecraft Bedrock addon development environment with essential configuration files

---
+++ README.md
@@ -0,0 +1,34 @@
+# Minecraft Bedrock Addons
+
+## Description du projet
+
+Ce projet est un modèle d'environnement de développement d'**addons** pour **Minecraft Bedrock**. Il permet d'automatiser certaines
+tâches, ou encore de valider les fichiers de configuration des addons avec des schémas JSON.
+
+## Table des matières
+
+- [Installation](#installation)
+- [Configuration des schémas JSON](#configuration-des-schémas-json)
+
+---
+
+## Installation
+
+### Prérequis
+
+- Minecraft Bedrock Edition.
+- Un éditeur de texte comme **Visual Studio Code** (VSCode) avec l'extension **JSON Schema**.
+- Installer **Node.js** et **npm** pour exécuter les scripts.
+- Une connexion internet pour récupérer les schémas JSON.
+
+### Étapes d'installation
+
+1. **Télécharger le zip** du projet avec le bouton vert **Code**.
+2. **Extraire le contenu** du zip dans un dossier de votre choix.
+3. **Ouvrir VSCode** et **ouvrir le dossier** du projet.
+
+## Configuration des schémas JSON
+
+Les schémas JSON permettent de valider les fichiers de configuration des addons. Dans le fichier `settings.json` situé dans le dossier `.vscode`, il y a une section
+lié à la configuration des schémas JSON. Ce fichier sera souvent mis à jour sur ce repository pour ajouter de nouveaux schémas ou en modifier,
+donc pensez à retélécharger le fichier `settings.json` régulièrement.
---
+++ just.config.ts
@@ -0,0 +1,90 @@
+import { argv, parallel, series, task, tscTask } from "just-scripts";
+import {
+  BundleTaskParameters,
+  CopyTaskParameters,
+  bundleTask,
+  cleanTask,
+  cleanCollateralTask,
+  copyTask,
+  coreLint,
+  mcaddonTask,
+  setupEnvironment,
+  ZipTaskParameters,
+  STANDARD_CLEAN_PATHS,
+  DEFAULT_CLEAN_DIRECTORIES,
+  getOrThrowFromProcess,
+  watchTask
+} from "@douarmc/core-build-tasks";
+import path from "path";
+import fs from "fs";
+
+setupEnvironment(path.resolve(__dirname, ".env")); // Charge les variables d'environnement du fichier .env
+
+const projectName = getOrThrowFromProcess("PROJECT_NAME"); // Récupère le nom du projet depuis les variables d'environnement
+
+// Paramètres des tâches de bundle pour le script.
+const bundleTaskOptions: BundleTaskParameters = {
+  entryPoint: path.join(__dirname, "./scripts/main.ts"), // Fichier d'entrée du script.
+  external: ["@minecraft/server", "@minecraft/server-ui"], // Dépendances externes du script.
+  outfile: path.resolve(__dirname, "./dist/scripts/main.js"), // Fichier de sortie du script en JavaScript.
+  minifyWhitespace: false, // Minifie le code en retirant les espaces inutiles.
+  sourcemap: true, // Génère un fichier de source map pour le script.
+  outputSourcemapPath: path.resolve(__dirname, "./dist/debug"), // Chemin de sortie du fichier de source map.
+};
+
+/**
+ * Fonction pour vérifier si un répertoire existe et contient du contenu.
+ * @param directory Le répertoire à vérifier.
+ * @returns 
+ */
+function hasDirectory(directory: string): boolean {
+  return fs.existsSync(directory) && fs.readdirSync(directory).length > 0;
+};
+
+/**
+ * Vérifie si un répertoire contient des fichiers TypeScript.
+ * @param directory Le répertoire à vérifier.
+ * @returns 
+ */
+function hasTypeScriptFiles(directory: string): boolean {
+  return fs.existsSync(directory) && fs.readdirSync(directory).some(file => file.endsWith('.ts'));
+};
+
+// Paramètres des tâches de copie des packs.
+const copyTaskOptions: CopyTaskParameters = {
+  copyToBehaviorPacks: hasDirectory(`./behavior_packs/${projectName}`) ? [`./behavior_packs/${projectName}`] : [],
+  copyToScripts: hasTypeScriptFiles(path.resolve(__dirname, "scripts")) ? ["./dist/scripts"] : [],
+  copyToResourcePacks: hasDirectory(`./resource_packs/${projectName}`) ? [`./resource_packs/${projectName}`] : [],
+};
+
+// Paramètres des tâches de création de fichier mcaddon.
+const mcaddonTaskOptions: ZipTaskParameters = {
+  ...copyTaskOptions, // Copie les options de copie.
+  outputFile: `./dist/packages/${projectName}.mcaddon`, // Chemin de sortie du fichier mcaddon.
+};
+
+// Vérifie si le répertoire de scripts contient des fichiers TypeScript.
+if (hasTypeScriptFiles(path.resolve(__dirname, "scripts"))) {
+  task("typescript", tscTask()); // Tâche de compilation des fichiers TypeScript.
+  task("bundle", bundleTask(bundleTaskOptions)); // Tâche de bundle du script.
+  task("build", series("typescript", "bundle")); // Tâche de build du projet (compilation + bundle).
+} else {
+  task("build", () => console.log("No TypeScript files found in the scripts directory.")); // Tâche de build du projet (aucun fichier TypeScript trouvé).
+}
+
+
+task("lint", coreLint(["scripts/**/*.ts"], argv().fix));
+task("clean-local", cleanTask(DEFAULT_CLEAN_DIRECTORIES));
+task("clean-collateral", cleanCollateralTask(STANDARD_CLEAN_PATHS));
+task("clean", parallel("clean-local", "clean-collateral"));
+task("copyArtifacts", copyTask(copyTaskOptions));
+task("package", series("clean-collateral", "copyArtifacts"));
+task(
+  "local-deploy",
+  watchTask(
+    ["scripts/**/*.ts", "behavior_packs/**/*.{json,lang,tga,ogg,png}", "resource_packs/**/*.{json,lang,tga,ogg,png}"],
+    series("clean-local", "build", "package")
+  )
+);
+task("createMcaddonFile", mcaddonTask(mcaddonTaskOptions));
+task("mcaddon", series("clean-local", "build", "createMcaddonFile"));
